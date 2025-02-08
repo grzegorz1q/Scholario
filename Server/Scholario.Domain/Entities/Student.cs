66df049bbf86c3154bab521495c88bf9b347dd80
Pafton <ptrojnar3@gmail.com>
2025-02-08 14:34:59 +0100

    Create new models

---
+++ Server/Scholario.Domain/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scholario.Domain.Entities
+{
+    public class Student : Person
+    {
+        public virtual Group Group { get; set; } = default!;
+        public int GroupId { get; set; }
+        public virtual Parent Parent { get; set; } = default!;
+        public int ParentId { get; set; }
+        public virtual ICollection<Grade> Grades { get; set; } = default!;
+    }
+}
---
+++ Server/Scholario.Domain/Entities/Subject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scholario.Domain.Entities
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public virtual Teacher Teacher { get; set; } = default!;
+        public int TeacherId { get; set; }
+        public virtual ICollection<Grade> Grades { get; set; } = default!;
+        public virtual ICollection<Group> Groups { get; set; } = default!;
+    }
+}
