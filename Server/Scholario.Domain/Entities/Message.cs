f6885e10118866355e3f78712391051876812847
Pafton <ptrojnar3@gmail.com>
2025-02-08 19:16:36 +0100

    Add new entites: - notificationTypes, - notification - message

---
+++ Server/Scholario.Domain/Entities/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scholario.Domain.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public virtual Person Sender { get; set; } = default!;
+        public int ReceiverId { get; set; } 
+        public virtual Person Receiver { get; set; } = default!;
+        public string Content { get; set; } = string.Empty;
+        public DateTime Sent { get; set; } 
+        public bool IsRead { get; set; } = false; 
+    }
+}
---
+++ Server/Scholario.Domain/Entities/NotificationType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scholario.Domain.Entities
+{
+    public enum NotificationType
+    {
+        GradeAssigned,
+        MessageReceived,
+        GeneralInfo
+    }
+}
