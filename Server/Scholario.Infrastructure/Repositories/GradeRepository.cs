1b295e8e8650c45d3fbccd7615a42376a11716bd
grzegorz1q <grzegorbaryla@gmail.com>
2025-02-11 12:05:04 +0100

    Implemented all repositories

---
+++ Server/Scholario.Infrastructure/Repositories/GradeRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scholario.Application.Interfaces.Repositories;
+using Scholario.Domain.Entities;
+using Scholario.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scholario.Infrastructure.Repositories
+{
+    public class GradeRepository : IGradeRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        public GradeRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task AddGrade(Grade grade)
+        {
+            if (grade == null)
+                throw new ArgumentNullException(nameof(grade));
+            await _appDbContext.Grades.AddAsync(grade);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteGrade(int id)
+        {
+            if (id < 0)
+                throw new ArgumentOutOfRangeException(nameof(id));
+            var grade = await _appDbContext.Grades.FirstOrDefaultAsync(g => g.Id == id);
+            if (grade == null)
+            {
+                throw new KeyNotFoundException($"Ocena o ID {id} nie została znaleziona.");
+            }
+            _appDbContext.Grades.Remove(grade);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Grade>> GetAllGrades()
+        {
+            return await _appDbContext.Grades.ToListAsync();
+        }
+
+        public async Task<Grade?> GetGrade(int id)
+        {
+            if (id < 0)
+                throw new ArgumentOutOfRangeException(nameof(id));
+            return await _appDbContext.Grades.FirstOrDefaultAsync(g => g.Id == id);
+        }
+
+        public async Task UpdateGrade(Grade grade)
+        {
+            if (grade == null)
+                throw new ArgumentNullException(nameof(grade));
+            _appDbContext.Grades.Update(grade);
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Server/Scholario.Infrastructure/Repositories/LessonHourRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scholario.Application.Interfaces.Repositories;
+using Scholario.Domain.Entities;
+using Scholario.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scholario.Infrastructure.Repositories
+{
+    public class LessonHourRepository : ILessonHourRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        public LessonHourRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task AddLessonHour(LessonHour lessonHour)
+        {
+            if (lessonHour == null)
+                throw new ArgumentNullException(nameof(lessonHour));
+            await _appDbContext.LessonHours.AddAsync(lessonHour);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteLessonHour(int id)
+        {
+            if (id < 0)
+                throw new ArgumentOutOfRangeException(nameof(id));
+            var lessonHour = await _appDbContext.LessonHours.FirstOrDefaultAsync(l => l.Id == id);
+            if (lessonHour == null)
+            {
+                throw new KeyNotFoundException($"Godzina lekcyjna o ID {id} nie został znaleziony.");
+            }
+            _appDbContext.LessonHours.Remove(lessonHour);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<LessonHour>> GetAllLessonHours()
+        {
+            return await _appDbContext.LessonHours.ToListAsync();
+        }
+
+        public async Task<LessonHour?> GetLessonHour(int id)
+        {
+            if (id < 0)
+                throw new ArgumentOutOfRangeException(nameof(id));
+            return await _appDbContext.LessonHours.FirstOrDefaultAsync(l => l.Id == id);
+        }
+
+        public async Task UpdateLessonHour(LessonHour lessonHour)
+        {
+            if (lessonHour == null)
+                throw new ArgumentNullException(nameof(lessonHour));
+            _appDbContext.LessonHours.Update(lessonHour);
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
