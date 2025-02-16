9485a9671ca4792b5819d54579b99318eb364d7b
Pafton <ptrojnar3@gmail.com>
2025-02-16 18:00:32 +0100

    Create entite for classroom

---
+++ Server/Scholario.Domain/Entities/Classroom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scholario.Domain.Entities
+{
+    public class Classroom
+    {
+        public int Id { get; set; }
+        public string roomNumber { get; set; } = string.Empty;
+        public ICollection<ScheduleEntry> scheduleEntries { get; set; } = default!;
+
+    }
+}
